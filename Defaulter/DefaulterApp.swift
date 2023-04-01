ae73e8a8e3cd4182137a142028a07af1394a41f4
kaascevich <swimmer-flowing-0s@icloud.com>
2023-04-01 10:24:51 -0400

    Initial Commit

---
+++ Defaulter/DefaulterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DefaulterApp.swift
+//  Defaulter
+//
+//  Created by Kaleb on 4/1/23.
+//
+
+import SwiftUI
+
+@main
+struct DefaulterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
